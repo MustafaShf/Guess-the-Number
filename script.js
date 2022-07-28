64fbed4ad788c007ab513b099436fa4b80e4d3f4
Ghulam Mustafa <89721754+MustafaShf@users.noreply.github.com>
2022-07-28 15:22:30 +0500

    error resove

--- script.js
+++ script.js
@@ -77,6 +77,7 @@ document.querySelector('.again').addEventListener('click', function() {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('.score').textContent = '20';
+    document.querySelector('.guess').value = '';
     document.querySelector('.number').textContent = '?';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15 rem ';
